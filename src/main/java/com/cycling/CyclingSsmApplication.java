237c47c3a71b3e35a2c418b344746b791f06fa3c
xpdxz <1059853272@qq.com>
2021-09-22 21:34:17 +0800

    集成swagger

--- src/main/java/com/cycling/CyclingSsmApplication.java
+++ src/main/java/com/cycling/CyclingSsmApplication.java
@@ -2,8 +2,10 @@ package com.cycling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
+@EnableOpenApi
 public class CyclingSsmApplication {
 
     public static void main(String[] args) {
