8a9c8c61a1bf7967e7e2b7c204298c1fe9c660ed
RainGoal <88344822@qq.com>
2021-09-22 19:11:24 +0800

    初始化后台项目

---
+++ src/main/java/com/cycling/CyclingSsmApplication.java
@@ -0,0 +1,13 @@
+package com.cycling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyclingSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyclingSsmApplication.class, args);
+    }
+
+}
