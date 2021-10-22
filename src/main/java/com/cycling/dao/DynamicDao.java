427a841243cba0512f473f1b03c70f5a6b576417
RainGoal <88344822@qq.com>
2021-10-22 10:04:52 +0800

    等待

--- src/main/java/com/cycling/dao/DynamicDao.java
+++ src/main/java/com/cycling/dao/DynamicDao.java
@@ -2,6 +2,8 @@ package com.cycling.dao;
 
 import com.cycling.pojo.Dynamic;
 
+import java.util.List;
+
 /**
  * @ClassName: DynamicDao
  * @Author: RainGoal
@@ -25,4 +27,12 @@ public interface DynamicDao {
      * @return: int
      */
     int deleteDynamic(Integer id);
+
+    /**
+     * 根据用户查询动态
+     * @author RainGoal
+     * @param username
+     * @return: java.util.List<com.cycling.pojo.Dynamic>
+     */
+    List<Dynamic> findDynamicByUser(String username);
 }
