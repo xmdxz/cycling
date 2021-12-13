fa456e83a16c49f75a1b24910a8bc54547da1b24
yuze <649946725@qq.com>
2021-12-13 16:17:15 +0800

    登录

--- src/main/java/com/cycling/utils/JWTUtils.java
+++ src/main/java/com/cycling/utils/JWTUtils.java
@@ -3,6 +3,7 @@ package com.cycling.utils;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.extern.log4j.Log4j2;
 
@@ -26,12 +27,19 @@ public class JWTUtils {
     /**
      * token过期时间
      */
-    private static final long EXPIRE_TIME = 60 * 60 * 24 * 7 * 1000;
+//    private static final long EXPIRE_TIME = 60 * 60 * 24 * 7 * 1000;
+    private static final long EXPIRE_TIME = 30 * 1000;
 
     /**
      * 刷新时间
      */
-    public static final long REFRESH_TOKEN_EXPIRE_TIME = 60 * 60 * 24 * 7 * 14 * 1000;
+//    public static final long REFRESH_TOKEN_EXPIRE_TIME = 60 * 60 * 24 * 7 * 14 * 1000;
+    public static final long REFRESH_TOKEN_EXPIRE_TIME = 180 *  1000;
+
+    /**
+     * token过期的状态码
+     */
+    public static final int TOKEN_EXPIRE_CODE = 10010;
 
     /**
      * 生成token
@@ -68,10 +76,18 @@ public class JWTUtils {
      * @date 2021/9/29 11:57 上午
      * @return: void
      */
-    public static boolean verify(String token) throws Exception {
-
-        DecodedJWT v = JWT.require(Algorithm.HMAC256(SIGN)).build().verify(token);
-        return true;
+    public static boolean verify(String token){
+        try {
+            DecodedJWT v = JWT.require(Algorithm.HMAC256(SIGN)).build().verify(token);
+            return true;
+        }catch (Exception e)
+        {
+            if(e instanceof TokenExpiredException)
+                throw new TokenExpiredException("token过期");
+            else{
+                return false;
+            }
+        }
 
     }
 
@@ -83,7 +99,8 @@ public class JWTUtils {
      * @date 2021/9/29 12:06 下午
      * @return: com.auth0.jwt.interfaces.DecodedJWT
      */
-    public static DecodedJWT getTokenInfo(String token) {
-        return JWT.require(Algorithm.HMAC256(SIGN)).build().verify(token);
+    public static com.auth0.jwt.interfaces.Claim getTokenInfo(String token, String name) {
+        DecodedJWT decode = JWT.decode(token);
+        return decode.getClaim(name);
     }
 }
